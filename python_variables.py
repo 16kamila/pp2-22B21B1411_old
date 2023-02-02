9b8648556c633974bd0bd3a1b85fd4a4ce56d248
16kamila <122874229+16kamila@users.noreply.github.com>
2023-02-02 12:04:39 +0600

    Add files via upload

---
+++ python_variables.py
@@ -0,0 +1,27 @@
+#1
+carname="volvo"
+
+#2
+x=50
+
+#3
+x=5
+y=10
+print(x+y)
+
+#4
+x=5
+y=10
+z=x+y
+print(z)
+
+#5
+myfirst_name="john"
+
+#6
+x=y=z="orange"
+
+#7
+def myfinc():
+    global x
+    x="fantastic"
